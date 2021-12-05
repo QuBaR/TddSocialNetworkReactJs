5cff3e8181707c0f1e8c11c444d715ed1662dcab
Robert Jansz <robert@webking.se>
2021-12-05 21:54:29 +0100

    Entity framework added PostsController added

---
+++ Webb/Data/SocialNetworkDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TddSocialNetwork.Model;
+
+namespace Webb.Data
+{
+    public class SocialNetworkDbContext : DbContext
+    {
+        public SocialNetworkDbContext (DbContextOptions<SocialNetworkDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TddSocialNetwork.Model.Post> Post { get; set; }
+    }
+}
--- Webb/Startup.cs
+++ Webb/Startup.cs
@@ -1,10 +1,12 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Webb.Data;
 
 namespace Webb
 {
@@ -28,6 +30,9 @@ namespace Webb
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.AddDbContext<SocialNetworkDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SocialNetworkDbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
